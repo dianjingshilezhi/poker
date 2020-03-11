99459fe8efe0048de6dc9f00a097497707c2cc9b
zhaoyu <1070717172@qq.com>
2020-03-11 21:02:23 +0800

    finall

--- Poker.py
+++ Poker.py
@@ -19,5 +19,37 @@ def analyse(pok):
     while i<4:
         if pok[i][1]==pok[i+1][1] and number[pok[i][0]][0]==number[pok[i+1][0]][0]-1:
             i=i+1
-        else:return
-    return 9,number[pok[4][0]][0]
+        else:
+            number2=number
+            for i in range(5):
+                number2[pok[i][0]][1]=number2[pok[i][0]][1]+1
+            for i in range(5):
+                if number2[pok[i][0]][1]==4:
+                    return 8,number2[pok[i][0]][0]#铁支
+            for i in range(5):
+                for m in range(5):
+                    if number2[pok[i][0]][1]==3 and number2[pok[m][0]][1]==2:
+                        return 7,number2[pok[i][0]][0]#葫芦
+            for i in range(4):
+                if pok[i][1]!=pok[i+1][1]:
+                    for i in range(4):
+                        if number[pok[i][0]][0]!=number[pok[i+1][0]][0]-1:
+                            for m in range(5):
+                                if number2[pok[m][0]][1] == 3:
+                                    return 4,number2[pok[m][0]][0]#三条
+                                else:
+                                    for i in range(5):
+                                        for m in range(5):
+                                            if pok[i][0]!=pok[m][0]and number2[pok[i][0]][1] == 2 and number2[pok[m][0]][1] == 2:
+                                                return 3, number2[pok[m][0]][0]  # 双对
+                                    for i in range(5):
+                                        if number2[pok[i][0]][1] == 2:
+                                            return 2, number2[pok[i][0]][0]  # 一对
+                                    return 1,number[pok[4][0]][0]#散牌
+
+                    return 5,number[pok[4][0]][0]#顺子
+            return 6,number[pok[4][0]][0]#同花
+
+
+    return 9,number[pok[4][0]][0]#同花顺
+
--- test_poker.py
+++ test_poker.py
@@ -4,5 +4,23 @@ class Mytest(unittest.TestCase):
     def test_Straightflush(self):
         test=poker(['2H','3H','4H','5H','6H'],['3D','4D','5D','6D','7D'])
         self.assertEquals(test,'black-win')
+    def test_4same(self):
+        test=poker(['3D','4D','5D','6D','7D'],['3D','3S','3H','3C','KS'])
+        self.assertEquals(test,'white-win')
+    def testhulu(self):
+        test=poker(['3D','3S','3H','7D','7H'],['5D','6D','9D','QD','KD'])
+        self.assertEquals(test,'white-win')
+    def test_santioa(self):
+        test=poker(['3D','3S','3H','7D','9H'],['5D','5H','9D','9H','KD'])
+        self.assertEquals(test,'white-win')
+    def test_twopair(self):
+        test=poker(['3D','3S','6H','7D','9H'],['5D','5H','9D','9H','KD'])
+        self.assertEquals(test,'black-win')
+    def test_pair(self):
+        test = poker(['3D', '3S', '6H', '9D', 'QC'], ['2H', '6C', '9D', 'JH', 'KD'])
+        self.assertEquals(test, 'white-win')
+    def testtie(self):
+        test=poker(['3H','3D','5S','9C','KD'],['3H','3D','5S','9C','KD'])
+        self.assertEquals(test,'tie')
 if __name__ == '__main__':
     mytest=Mytest
